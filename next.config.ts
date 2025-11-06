aaa79cc18fd28478917568daf8c28848c47f7824
animeshj2132 <ajangir52@gmail.com>
2025-11-06 17:08:36 +0530

    redirection

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.tracepoint.vercel.app",
+          },
+        ],
+        destination: "https://tracepoint.vercel.app/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
